33987c7a2ee376c0365134dd74317e719bd06710
Florian Geerken <florian.geerken@gmail.com>
2024-10-14 16:38:04 +0200

    last two sections day 3

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -67,10 +67,28 @@ def analyse_data(data_source):
 
     daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
 
-    graph_data = {
-        'standard deviation by day': daily_standard_deviation,
-    }
-    views.visualize(graph_data)
+    return daily_standard_deviation
+
+def compute_standard_deviation_by_day(data):
+    means_by_day = map(models.daily_mean, data)
+    means_by_day_matrix = np.stack(list(means_by_day))
+
+    daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
+    return daily_standard_deviation
+
+def analyse_data(data_source):
+    """Calculate the standard deviation by day between datasets
+    Gets all the inflammation csvs within a directory, works out the mean
+    inflammation value for each day across all datasets, then graphs the
+    standard deviation of these means."""
+    data = data_source.load_inflammation_data()
+    daily_standard_deviation = compute_standard_deviation_by_day(data)
+
+    return daily_standard_deviation
+
+
+
+
 
 # CONTROLLER
 # _, extension = os.path.splitext(infiles[0])
