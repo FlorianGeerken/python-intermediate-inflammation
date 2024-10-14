fef735b334aa4f0e54494ece5810c08cca250d58
Florian Geerken <florian.geerken@gmail.com>
2024-10-14 17:00:11 +0200

    changes to controller

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -28,6 +28,21 @@ def main(args):
 
         views.visualize(view_data)
 
+        # if args.full_data_analysis:
+        #     _, extension = os.path.splitext(infiles[0])
+        #     if extension == '.json':
+        #         data_source = JSONDataSource(os.path.dirname(infiles[0]))
+        #     elif extension == '.csv':
+        #         data_source = CSVDataSource(os.path.dirname(infiles[0]))
+        #     else:
+        #         raise ValueError(f'Unsupported file format: {extension}')
+        #     data_result = analyse_data(data_source)
+        #     graph_data = {
+        #         'standard deviation by day': data_result,
+        #     }
+        #     views.visualize(graph_data)
+        # return
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -47,18 +62,5 @@ if __name__ == "__main__":
 
     main(args)
 
-if args.full_data_analysis:
-    _, extension = os.path.splitext(infiles[0])
-    if extension == '.json':
-        data_source = JSONDataSource(os.path.dirname(infiles[0]))
-    elif extension == '.csv':
-        data_source = CSVDataSource(os.path.dirname(infiles[0]))
-    else:
-        raise ValueError(f'Unsupported file format: {extension}')
-    data_result = analyse_data(data_source)
-    graph_data = {
-        'standard deviation by day': data_result,
-    }
-    views.visualize(graph_data)
-    return
+
 
